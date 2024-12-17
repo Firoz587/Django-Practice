59d2e34537a4e903d04c4027d32cf2bc347447e2
Firoz <mdfirozislam940@gmail.com>
2024-12-18 02:13:01 +0600

    mid_term

--- mid_term/manage.py
+++ mid_term/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'module_19_5.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mid_term.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- mid_term/mid_term/asgi.py
+++ mid_term/mid_term/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for module_19_5 project.
+ASGI config for mid_term project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'module_19_5.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mid_term.settings')
 
 application = get_asgi_application()
